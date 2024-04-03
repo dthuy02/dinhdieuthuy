779f53a8a4b0065d1ef41872abade44b1a664ed5
dthuy02 <dinhdieuthuy1512@gmail.com>
2024-04-03 17:46:52 +0700

    commit lan 2

--- DinhDieuThuy.py
+++ DinhDieuThuy.py
@@ -5,3 +5,17 @@
 #     > 
 #     Nam đế cư
 #     Nam quốc sơn hà
+
+import sys
+# Mở file để đọc
+file_path = "test.txt"
+with open(file_path, "read") as file:
+    # Đọc từng dòng trong file và lưu vào danh sách
+    lines = file.readlines()
+
+# In danh sách dòng theo thứ tự đảo ngược
+for line in reversed(lines):
+    print(line.strip())
+
+# Đóng file sau khi đọc xong
+file.close()
