b7fa35d21fe0fa5773df7a4cdc3b1ff8c8ffd609
dthuy02 <dinhdieuthuy1512@gmail.com>
2024-04-03 17:27:50 +0700

    commit lan 1

---
+++ DinhDieuThuy.py
@@ -0,0 +1,7 @@
+# Đề bài: Đọc 1 file văn bản gồm nhiều dòng 
+#         Ghi ra màn hình theo thứ tự ngược lại của các dòng
+# VD: Nam quốc sơn hà 
+#     Nam đế cư
+#     > 
+#     Nam đế cư
+#     Nam quốc sơn hà
